ed76a152ee59c9227423fe5467f772430cb951dc
wood.wu <kilokingw@gmail.com>
2023-03-14 18:53:20 +0800

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <div className='py-4 bg-[#e4e4e400] absolute w-full top-0 z-50 pr-3'>
       <img src={process.env.PUBLIC_URL+'/images/open/navbar_logo.png'} alt="" className=' absolute top-0 left-1/2 transform -translate-x-1/2  max-w-full w-[80px] lg:w-[110px] hidden' />
       <div className='flex gap-5 justify-start items-end ml-6'>
-        <a href="https://www.facebook.com/"  target="_blank" rel="noreferrer">
+        <a href="https://www.facebook.com/people/%E6%98%95%E6%9A%89%E8%A9%A0%E7%BF%A0/100086047560305/"  target="_blank" rel="noreferrer">
           <div className='flex flex-col items-center justify-end gap-2 '>
             <img src={process.env.PUBLIC_URL+'/images/open_v2/fbicon.png'} alt="" className='w-[25px] lg:w-[40px] transition-all duration-500'/>
             <div className=' tracking-widest text-[#223553] font-bold hidden md:block'>粉絲專頁</div>
