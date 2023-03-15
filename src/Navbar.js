cc5456e09342fe2ce4cdbc7058712ba028b410d6
wood.wu <kilokingw@gmail.com>
2023-03-15 13:35:55 +0800

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,12 +12,6 @@ function Navbar() {
           </div>
           
         </a>
-        <a href="https://www.facebook.com/"  target="_blank" rel="noreferrer">
-          <div className='flex flex-col items-center gap-2'>
-            <img src={process.env.PUBLIC_URL+'/images/open_v2/fbmsgicon.png'} alt="" className='w-[26px] lg:w-[40px] transition-all duration-500'/>
-            <div className=' tracking-widest text-[#223553] font-bold hidden md:block'>FB諮詢</div>
-          </div>
-        </a>
         <a href="https://www.google.com/maps?ll=23.05468,120.211805&z=14&t=m&hl=zh-TW&gl=TW&mapclient=embed&q=%E6%96%B0%E5%AE%85+709%E5%8F%B0%E5%8D%97%E5%B8%82%E5%AE%89%E5%8D%97%E5%8D%80" target="_blank" rel="noreferrer">
           <div className='flex flex-col items-center gap-2'>
             <img src={process.env.PUBLIC_URL+'/images/open_v2/mapicon.png'} alt="" className='w-[20px] lg:w-[32px] transition-all duration-500'/>
